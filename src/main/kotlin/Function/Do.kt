59b0de99a670fbccdd0383a6292fc92cc8a54b1f
Your Sandeep5080 <sandeepreddy4366@gmail.com>
2021-12-14 17:00:08 +0530

    initial commit

--- src/main/kotlin/Function/Do.kt
+++ src/main/kotlin/Function/Do.kt
@@ -1,2 +1,18 @@
 package Function
 
+fun main(args : Array<String>) {
+
+    var i : Int = 1
+
+
+
+     do {
+         if (i %2 == 0) {
+             println(i)
+         }
+         i++
+         println(i)
+         i++
+
+     } while (i <= 10)
+     }
